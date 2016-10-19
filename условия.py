230b483d6e5ed2b66dfe2c350bfddfc7e26bfc66
TimurZhetpisov <volpone84@ya.ru>
2016-10-19 20:57:11 +0300

    Add files via upload

---
+++ условия.py
@@ -0,0 +1,10 @@
+
+a = int(input())
+b = int(input())
+c = int(input())
+if a * b == c:
+    print ('Обладает 1 свойством')
+if a / b == c:
+        print ('Обладает 2 свойством')
+else:
+    print ('Не обладает свойствами')
